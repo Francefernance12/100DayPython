5416e660d19e003b5a3e1c349b454815d6bf5632
theki <fa01.arias@gmail.com>
2023-12-01 22:51:52 -0500

    Day 15 Coffee Machine

---
+++ day_15/coffeeMachine.py
@@ -0,0 +1,39 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+# TODO: 1. print report
+# TODO: 2. check if resources is sufficient?
+# TODO: 3. process coin
+# TODO: 4. check if transactions is successful?
+# TODO: 5. Make the coffee.
+
