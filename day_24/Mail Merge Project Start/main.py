a998f68723a01fcb8a5b44fdf7172633dcbd0861
Francerfernance <thekidofsteel01@gmail.com>
2024-01-04 00:14:00 -0500

    Day 24 Mail Merging

---
+++ day_24/Mail Merge Project Start/main.py
@@ -0,0 +1,16 @@
+
+PLACEHOLDER = "[name]"
+
+with open("Output/ReadyToSend/Names/invited_names.txt") as invited_names:
+    names = invited_names.readlines()
+
+
+with open("Input/Letters/starting_letter.txt") as letters:
+    letter_contents = letters.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", "w") as completed_letter:
+            completed_letter.write(new_letter)
+
+
