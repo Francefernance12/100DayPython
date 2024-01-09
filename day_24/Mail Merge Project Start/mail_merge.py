366b8d9570e3f373c821414436681de7808a90ce
Francerfernance <thekidofsteel01@gmail.com>
2024-01-08 22:01:01 -0500

    Day 24 Mail Merging

--- day_24/Mail Merge Project Start/mail_merge.py
+++ day_24/Mail Merge Project Start/mail_merge.py
@@ -1,10 +1,12 @@
 
 PLACEHOLDER = "[name]"
 
+# sample letter
 with open("Output/ReadyToSend/Names/invited_names.txt") as invited_names:
     names = invited_names.readlines()
 
 
+# replacing the placeholder with the names
 with open("Input/Letters/starting_letter.txt") as letters:
     letter_contents = letters.read()
     for name in names:
