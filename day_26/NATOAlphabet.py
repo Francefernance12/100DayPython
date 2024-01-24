aacf3c3e3a6b3c0ee90dc8dbc2fbd81d1f840b50
Francerfernance <thekidofsteel01@gmail.com>
2024-01-23 20:04:21 -0500

    Day 26 NATO Alphabet

--- day_26/NATOAlphabet.py
+++ day_26/NATOAlphabet.py
@@ -3,10 +3,15 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(phonetic_dict)
+print(phonetic_dict)
 
-user_input = input("Enter a word: ").upper()
-lists_letters = [character for character in user_input]
-create_code = [phonetic_dict[letter] for letter in lists_letters if letter in phonetic_dict]
-print(lists_letters)
-print(create_code)
+input_is_typed = False
+while not input_is_typed:
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+        print(output_list)
+        input_is_typed = True
+    except KeyError:
+        print("Sorry, that is not in the dictionary")
+
